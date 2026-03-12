c15e9a80b7a0aee6d5652a59d8a9be8e72bb37a6
ilfpns <yeomsh09@gmail.com>
2026-03-13 02:15:30 +0900

    feat: Add ESP-NOW initialization and receive functions

--- W.I.F.I-STA-Embedded/components/espnow/espnowSTA.c
+++ W.I.F.I-STA-Embedded/components/espnow/espnowSTA.c
@@ -1,2 +1,29 @@
 #include "espnowSTA.h"
 
+// temp MAC addr
+uint8_t TX_MAC_ADDRESS[6] = {0x24, 0x6F, 0x28, 0xAB, 0xCD, 0xEF}; 
+
+static const char *TAG = "ESPNOW-RX";
+
+void espnow_recv_cb(const esp_now_recv_info_t *esp_now_info, const uint8_t *data, int data_len) {
+    if (data_len == sizeof(espnow_payload_t)) {
+        espnow_payload_t *payload = (espnow_payload_t *)data;
+        
+        if (payload->command == 1) {
+            ESP_LOGI(TAG, "'1' 수신, 슬립 모드 해제");
+        }
+    }
+}
+
+esp_err_t espnow_init_setup(void) {
+    esp_err_t err = esp_now_init();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "ESP-NOW 초기화 실패");
+        return err;
+    }
+    
+    ESP_ERROR_CHECK(esp_now_register_recv_cb(espnow_recv_cb));
+    ESP_LOGI(TAG, "ESP-NOW 단독 세팅 완료!");
+    
+    return ESP_OK;
+}
--- W.I.F.I-STA-Embedded/components/espnow/espnowSTA.h
+++ W.I.F.I-STA-Embedded/components/espnow/espnowSTA.h
@@ -1,2 +1,18 @@
 #pragma once 
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "esp_wifi.h"
+#include "esp_now.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#define WIFI_CHANNEL 6
+
+typedef struct {
+    uint8_t command; 
+} espnow_payload_t;
